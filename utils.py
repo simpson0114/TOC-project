11a0efbdbd12fb99b3676bcefbcf6c53f26f3759
simpson0114 <simpson880114@gmail.com>
2019-12-06 13:49:47 +0800

    debug

--- utils.py
+++ utils.py
@@ -12,9 +12,8 @@ def send_text_message(reply_token, text):
     line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
     return "OK"
 
-
 def send_image_url(reply_token, img_url):
-	line_bot_api = LineBotApi(channel_access_token)
+	
     return "OK"
 """
 def send_button_message(id, text, buttons):
