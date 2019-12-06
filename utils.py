5fe99fcb87a22db3d8fa6d1c4ba07e71504b5508
simpson0114 <simpson880114@gmail.com>
2019-12-06 14:29:37 +0800

    debug

--- utils.py
+++ utils.py
@@ -7,15 +7,17 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSend
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
 
 
+def send_image_url(reply_token, image_url):
+	line_bot_api = LineBotApi(channel_access_token)
+    line_bot_api.reply_message(reply_token, ImageSendMessage(original_content_url=image_url, preview_image_url=image_url))
+    return "image sent"
+
 def send_text_message(reply_token, text):
     line_bot_api = LineBotApi(channel_access_token)
     line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
     return "OK"
 
-def send_image_url(reply_token, image_url):
-	line_bot_api = LineBotApi(channel_access_token)
-    line_bot_api.reply_message(reply_token, ImageSendMessage(original_content_url=image_url, preview_image_url=image_url))
-    return "image sent"
+
 """
 def send_button_message(id, text, buttons):
     pass
