71264dac21973b108167a5035f38d1427beff2b1
simpson0114 <simpson880114@gmail.com>
2019-12-07 14:36:45 +0800

    debug

--- utils.py
+++ utils.py
@@ -20,19 +20,19 @@ def send_image_url(reply_token, image_url):
 	
 def send_food_message(reply_token):
     line_bot_api = LineBotApi(channel_access_token)
-   	line_bot_api.reply_message(reply_token, TextSendMessage(text=choice(food)))
+    line_bot_api.reply_message(reply_token, TextSendMessage(text=choice(food)))
     return "OK"
 
 def send_allfood_message(reply_token):
-	text = ""
-	line_bot_api = LineBotApi(channel_access_token)
-	for x in range(len(food)): 
-		text += food[x] 
-	line_bot_api.reply_message(reply_token, TextSendMessage(text=food[0:-1]))
-	return "OK"
+    text = ""
+    line_bot_api = LineBotApi(channel_access_token)
+    for x in range(len(food)): 
+        text += food[x] 
+    line_bot_api.reply_message(reply_token, TextSendMessage(text=food[0:-1]))
+    return "OK"
 
 def add_food_message(recieved_message):
-	food.append(recieved_message)
+    food.append(recieved_message)
 
 '''
 def send_button_message(id, text, buttons):
