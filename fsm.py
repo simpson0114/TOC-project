07d6ee888476e0468ed2ac59c6ee04dbea84db76
simpson0114 <simpson880114@gmail.com>
2019-12-07 11:39:21 +0800

    debug

--- fsm.py
+++ fsm.py
@@ -1,9 +1,10 @@
 from transitions.extensions import GraphMachine
 
-from utils import send_text_message, send_image_url
+from utils import send_text_message, send_image_url, send_food_message
 from random import choice
 
-food = ['鴨肉飯', '乾麵', '港式燒臘', '鍋燒意麵', '炒飯', '拉麵', '餛飩麵']
+
+photo = ['https://images.zi.org.tw/ireneslife/2018/08/23222608/1535034368-95cf834c4d3687e1347ed20f3cdb7cab.jpg',]
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
@@ -11,7 +12,7 @@ class TocMachine(GraphMachine):
 
     def is_going_to_state1(self, event):
         text = event.message.text
-        return text.lower() == "吃什麼"
+        return text[0..5] == "抽食物"
 
     def is_going_to_state2(self, event):
         text = event.message.text
@@ -20,8 +21,7 @@ class TocMachine(GraphMachine):
     def on_enter_state1(self, event):
         print("I'm entering state1")
         reply_token = event.reply_token
-        send_text_message(reply_token, choice(food))
-        send_image_url(reply_token, 'https://images.zi.org.tw/ireneslife/2018/08/23222608/1535034368-95cf834c4d3687e1347ed20f3cdb7cab.jpg')
+        send_food_message(reply_token, choice(food))
         self.go_back()
 
     def on_exit_state1(self):
@@ -30,7 +30,7 @@ class TocMachine(GraphMachine):
     def on_enter_state2(self, event):
         print("I'm entering state2")
         reply_token = event.reply_token
-        send_image_url(reply_token, 'https://images.zi.org.tw/ireneslife/2018/08/23222608/1535034368-95cf834c4d3687e1347ed20f3cdb7cab.jpg')
+        send_image_url(reply_token, photo[0])
         self.go_back()
 
     def on_exit_state2(self):
--- utils.py
+++ utils.py
@@ -2,20 +2,26 @@ import os
 
 from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
-
+from random import choice
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+food = ['鴨肉飯', '乾麵', '港式燒臘', '鍋燒意麵', '炒飯', '拉麵', '餛飩麵']
 
 
 def send_text_message(reply_token, text):
     line_bot_api = LineBotApi(channel_access_token)
-    line_bot_api.push_message(reply_token, TextSendMessage(text=text))
+    line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
     return "OK"
 
 def send_image_url(reply_token, image_url):
 	line_bot_api = LineBotApi(channel_access_token)
 	message = ImageSendMessage(original_content_url=image_url, preview_image_url=image_url)
-	line_bot_api.psuh_message(reply_token, message)
+	line_bot_api.reply_message(reply_token, message)
+	
+def send_food_message(reply_token, text):
+    line_bot_api = LineBotApi(channel_access_token)
+    line_bot_api.reply_message(reply_token, TextSendMessage(text=choice(food)))
+    return "OK"
 
 '''
 def send_button_message(id, text, buttons):
